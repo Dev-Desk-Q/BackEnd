eded2293de82597a9793b3fb4aee0d6505a16215
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-09-16 19:57:23 -0400

    Cleaned up server.js on sqlite3 error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-// const authRouter = require("../users/auth-router.js");
+
 // const usersRouter = require("./api/users-router");
 const restricted = require("./middleware/restrict");
 
@@ -12,7 +12,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use("/api/auth", authRouter);
+
 // server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
