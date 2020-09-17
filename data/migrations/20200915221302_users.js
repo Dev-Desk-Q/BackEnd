cc80ce3c210389dd045ec1414b52f141fa60fb1f
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-09-17 19:46:26 -0400

    Updates to ticket table including assigned, assigned_to, user_id

--- data/migrations/20200915221302_users.js
+++ data/migrations/20200915221302_users.js
@@ -1,8 +1,42 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("users", table => {
+    table.increments().primary();
+    table
+      .text("username")
+      .notNull()
+      .unique();
+    table.text("role").notNull();
+    table.text("password").notNull();
+  });
 
-exports.up = function(knex) {
-  
+  await knex.schema.createTable("tickets", table => {
+    table.increments();
+    table.text("title").notNull();
+    table.text("description").notNull();
+    table.text("tried").notNull();
+    table.text("category").notNull();
+    table.bool("completed").defaultTo(false);
+    table.bool("assigned").defaultTo(false);
+    table
+      .integer("assigned_to")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("tickets")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("tickets");
+  await knex.schema.dropTableIfExists("users");
 };
